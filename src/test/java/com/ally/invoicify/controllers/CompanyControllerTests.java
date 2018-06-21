ad7cef456756ad3427d529e0420f4721bf038357
RZ98K0@ALLYIPOC.COM <RZ98K0@ALLYIPOC.COM>
2018-06-21 16:09:14 -0400

    created company controller tests

---
+++ src/test/java/com/ally/invoicify/controllers/CompanyControllerTests.java
@@ -0,0 +1,14 @@
+package com.ally.invoicify.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CompanyControllerTests {
+
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+
+}
