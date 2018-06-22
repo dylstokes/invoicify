93982946c2f4415f7c3fd03d83405ffa0528fd40
dylstokes <Dylan.Stokes@ally.com>
2018-06-22 13:41:21 -0400

    removed unused imports

--- src/main/java/com/ally/invoicify/controllers/InvoiceController.java
+++ src/main/java/com/ally/invoicify/controllers/InvoiceController.java
@@ -1,9 +1,5 @@
 package com.ally.invoicify.controllers;
 
-import java.sql.Date;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,14 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ally.invoicify.models.BillingRecord;
 import com.ally.invoicify.models.Invoice;
-import com.ally.invoicify.models.InvoiceLineItem;
 import com.ally.invoicify.models.InvoiceView;
-import com.ally.invoicify.repositories.BillingRecordRepository;
-import com.ally.invoicify.repositories.CompanyRepository;
-import com.ally.invoicify.repositories.InvoiceRepository;
-import com.ally.invoicify.services.FlatFeeBillingRecordServiceImpl;
 import com.ally.invoicify.services.InvoiceServiceImpl;
 
 @RestController
