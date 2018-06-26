cba24b3f5a991f920cdfb5540ffa3eeb0b5c51ea
RZ98K0@ALLYIPOC.COM <RZ98K0@ALLYIPOC.COM>
2018-06-26 11:29:21 -0400

    Added user repository class

---
+++ src/main/java/com/ally/invoicify/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ally.invoicify.repositories;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
